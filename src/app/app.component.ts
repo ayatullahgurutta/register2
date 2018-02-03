a9dcd723ae7bb91e4ce8e6383b45977af5982d53
Mohammad Badrullami <mohammad.badrullami@gmail.com>
2018-02-03 13:53:39 +0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Headers, RequestOptions } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Register';
+  url = 'https://api.mesosfer.com/api/v2/users';
+  userlist = '';
+
+  constructor(private http: HttpClient) {} 
+  
+  getUser() {
+  	this.http.get(this.url, {
+  	  headers: {
+		"Content-Type": "application/json",
+		"X-Mesosfer-AppId": "tzVOewKnss",
+		"X-Mesosfer-AppKey": "ACNRwDdYOzGekYkDPbd1ffNpEPH0nr8J"
+	  }}).subscribe(
+   		(data: Response) => {
+      	  this.userlist = data.results;
+      	}
+      );
+  }
+
+  ngOnInit() {
+  	this.getUser();
+  }
+
+  regUser(regFirst: string, regLast: string, regMail: string, regPass: string) {
+  	let testData = {firstname: regFirst, lastname: regLast, email: regMail, password: regPass};
+  	this.http.post(this.url, 
+  	  JSON.stringify(testData), {
+  	  headers: {
+		"Content-Type": "application/json",
+		"X-Mesosfer-AppId": "tzVOewKnss",
+		"X-Mesosfer-AppKey": "ACNRwDdYOzGekYkDPbd1ffNpEPH0nr8J"
+	  }}).subscribe(
+   		(data: Response) => {
+      	  this.getUser();
+      	}
+      );
+  }
+
 }
