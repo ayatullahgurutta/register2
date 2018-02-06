ebe932bd11d05eea2558cc58a69768ef45a6a486
ayatullahgurutta <33905442+ayatullahgurutta@users.noreply.github.com>
2018-02-06 13:17:12 +0700

    Fix results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,8 +21,8 @@ export class AppComponent implements OnInit {
 		"X-Mesosfer-AppId": "tzVOewKnss",
 		"X-Mesosfer-AppKey": "ACNRwDdYOzGekYkDPbd1ffNpEPH0nr8J"
 	  }}).subscribe(
-   		(data: Response) => {
-      	  this.userlist = data.results;
+   		(data) => {
+          this.userlist = data['results'];
       	}
       );
   }
